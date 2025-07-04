200c521263e41ff09b1792918df28fc8025578dc
Anton Efimow <anton@efimow.name>
2025-07-04 11:06:24 +0200

    code review

--- src/network_client.ts
+++ src/network_client.ts
@@ -6,92 +6,44 @@ import {createConnectTransport} from "@connectrpc/connect-web";
 import {keccak_256} from "@noble/hashes/sha3";
 
 import {
-    CreatePayInRequest, CreatePayInResponse,
-    CreatePaymentRequest, CreatePaymentResponse, GetKycDataRequest, GetKycDataResponse,
-    GetPayoutQuoteRequest,
-    GetPayoutQuoteResponse,
-    NetworkService, UpdatePayoutRequest, UpdatePayoutResponse,
-    UpdateQuoteRequest,
-    UpdateQuoteResponse
+    NetworkService
 } from "./gen/network/network_pb";
 import {} from "./gen/network/provider_pb"
 
-export default class NetworkClient {
-    public client: Client<typeof NetworkService>;
+export default function createNetworkClient(endpoint: string, signer: SignerFunction) {
+    let customFetch: typeof global.fetch;
 
-    constructor(endpoint: string, signer: SignerFunction) {
-        let customFetch: typeof global.fetch;
+    customFetch = async (r, init) => {
+        if (!(init?.body instanceof Uint8Array)) {
+            throw "unsupported body type";
+        }
 
-        customFetch = async (r, init) => {
-            if (!(init?.body instanceof Uint8Array)) {
-                throw "unsupported body type";
-            }
-            const bodyText = new TextDecoder().decode(init.body);
+        const ts = Date.now();
+        // 64‑bit little‑endian timestamp
+        const tsBuf = Buffer.alloc(8);
+        tsBuf.writeBigInt64LE(BigInt(ts));
 
-            const ts = Date.now();
-            // 64‑bit little‑endian timestamp
-            const tsBuf = Buffer.alloc(8);
-            tsBuf.writeBigInt64LE(BigInt(ts));
+        const msgBuf = Buffer.concat([tsBuf, Buffer.from(init.body)]);
+        const hash = keccak_256(msgBuf);
+        const hashHex = Buffer.from(hash).toString("hex");
 
-            const msgBuf = Buffer.concat([tsBuf, Buffer.from(bodyText)]);
-            const hash = keccak_256(msgBuf);
-            const hashHex = Buffer.from(hash).toString("hex");
+        const sig = await signer(hashHex);
 
-            const sig = await signer(hashHex);
+        const headers = new Headers(init?.headers);
+        headers.append("X-Signature", sig.signature);
+        headers.append("X-PublicKey", sig.publicKey);
+        headers.append("X-Signature-Timestamp", ts.toString());
 
-            const headers = new Headers(init?.headers);
-            headers.append("X-Signature", sig.signature);
-            headers.append("X-PublicKey", sig.publicKey);
-            headers.append("X-Signature-Timestamp", ts.toString());
+        const modifiedInit: RequestInit = {...init, headers};
+        return fetch(r, modifiedInit)
+    };
 
-            const modifiedInit: RequestInit = {...init, headers};
-            return fetch(r, modifiedInit)
-        };
+    const transport = createConnectTransport({
+        baseUrl: endpoint,
+        fetch: customFetch,
+    });
 
-        const transport = createConnectTransport({
-            baseUrl: endpoint,
-            fetch: customFetch,
-        });
-
-        this.client = createClient(NetworkService, transport);
-    }
-
-    public async updateQuote(
-        request: UpdateQuoteRequest,
-    ): Promise<UpdateQuoteResponse> {
-        return this.client.updateQuote(request);
-    }
-
-    public async getPayoutQuote(
-        request: GetPayoutQuoteRequest,
-    ): Promise<GetPayoutQuoteResponse> {
-        return this.client.getPayoutQuote(request);
-    }
-
-    public async createPayment(
-        request: CreatePaymentRequest,
-    ): Promise<CreatePaymentResponse> {
-        return this.client.createPayment(request);
-    }
-
-    public async createPayIn(
-        request: CreatePayInRequest,
-    ): Promise<CreatePayInResponse> {
-        return this.client.createPayIn(request);
-    }
-
-    public async updatePayout(
-        request: UpdatePayoutRequest,
-    ): Promise<UpdatePayoutResponse> {
-        return this.client.updatePayout(request);
-    }
-
-    public async getKycData(
-        request: GetKycDataRequest,
-    ): Promise<GetKycDataResponse> {
-        return this.client.getKycData(request);
-    }
-
+    return createClient(NetworkService, transport);
 }
 
 /**
