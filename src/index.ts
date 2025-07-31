769e5fc6b0cbd76e385b31ccc6e746242f006ef7
Anton Efimow <anton@efimow.name>
2025-07-31 14:59:47 +0200

    fix_proto_structure (#18)
    * fix_proto_structure
    
    * add recipient api re-export
    
    * polishing imports
    
    ---------
    
    Co-authored-by: stepan_romankov <stepan@romankov.name>

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ export * from "./service/node"
 export { connectNodeAdapter as nodeAdapter} from "@connectrpc/connect-node";
 export type {HandlerContext} from "@connectrpc/connect";
 
-export * from './common/gen/common/common_pb'
-export * from './common/gen/common/payment_method_pb'
-export * from './common/gen/network/provider_pb'
-export * from './common/gen/network/network_pb'
+export * from './common/gen/tzero/v1/common/common_pb'
+export * from './common/gen/tzero/v1/common/payment_method_pb'
+export * from './common/gen/tzero/v1/payment/provider_pb'
+export * from './common/gen/tzero/v1/payment/network_pb'
 
-export * as PaymentIntentProvider from './common/gen/payment_intent/provider/provider_pb'
-export * as PaymentIntentRecipient from './common/gen/payment_intent/recipient/recipient_pb'
+export * as PaymentIntentProvider from './common/gen/tzero/v1/payment_intent/provider/provider_pb'
+export * as PaymentIntentRecipient from './common/gen/tzero/v1/payment_intent/recipient/recipient_pb'
